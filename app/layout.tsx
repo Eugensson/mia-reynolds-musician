9c301d58d71c5d5d5a5e47fc19ca76d29f2712db
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-20 12:02:05 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Alex_Brush, Montserrat } from "next/font/google";
+
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+
+import "./globals.css";
+
+const alexBrush = Alex_Brush({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-alexBrush",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+
+export const metadata: Metadata = {
+  title: "Mia Reynolds | Composer & Guitarist",
+  description:
+    "Discover Mia Reynolds, a talented composer and guitarist creating soulful music that blends classical artistry with modern expression. Explore original compositions, live performances, and inspiring guitar works.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${alexBrush.variable} ${montserrat.variable} antialiased relative overflow-x-hidden`}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,414 @@
+export const events = [
+  {
+    id: "1",
+    date: {
+      month: "Oct",
+      day: "20",
+    },
+    location: {
+      city: "Los Angeles",
+      country: "USA",
+      address: "LA Convention Center",
+    },
+    priceRange: "60-250$",
+  },
+  {
+    id: "2",
+    date: {
+      month: "Oct",
+      day: "25",
+    },
+    location: {
+      city: "Rio de Janeiro",
+      country: "Brazil",
+      address: "Sambadrome",
+    },
+    priceRange: "40-200$",
+  },
+  {
+    id: "3",
+    date: {
+      month: "Oct",
+      day: "30",
+    },
+    location: {
+      city: "Paris",
+      country: "France",
+      address: "Palais Omnisports de Paris",
+    },
+    priceRange: "70-300$",
+  },
+  {
+    id: "4",
+    date: {
+      month: "Nov",
+      day: "01",
+    },
+    location: {
+      city: "Berlin",
+      country: "Germany",
+      address: "Berlin Exhibition Hall",
+    },
+    priceRange: "55-280$",
+  },
+  {
+    id: "5",
+    date: {
+      month: "Nov",
+      day: "05",
+    },
+    location: {
+      city: "Athens",
+      country: "Greece",
+      address: "Acropolis Amphitheater",
+    },
+    priceRange: "45-220$",
+  },
+  {
+    id: "6",
+    date: {
+      month: "Nov",
+      day: "05",
+    },
+    location: {
+      city: "Bucharest",
+      country: "Romania",
+      address: "National Arena",
+    },
+    priceRange: "40-250$",
+  },
+  {
+    id: "7",
+    date: {
+      month: "Nov",
+      day: "05",
+    },
+    location: {
+      city: "Warsaw",
+      country: "Poland",
+      address: "PGE Narodowy",
+    },
+    priceRange: "55-280$",
+  },
+];
+
+export const albums = [
+  {
+    id: "1",
+    img: "/assets/albums/cover/cover-1.png",
+    title: "Album 1",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "2",
+    img: "/assets/albums/cover/cover-2.png",
+    title: "Album 2",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "3",
+    img: "/assets/albums/cover/cover-3.png",
+    title: "Album 3",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "4",
+    img: "/assets/albums/cover/cover-4.png",
+    title: "Album 4",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "5",
+    img: "/assets/albums/cover/cover-5.png",
+    title: "Album 5",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "6",
+    img: "/assets/albums/cover/cover-6.png",
+    title: "Album 6",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+  {
+    id: "7",
+    img: "/assets/albums/cover/cover-7.png",
+    title: "Album 7",
+    tracks: [
+      {
+        title: "Serenade in D Major",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Midnight Ride",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Whisper of the Wind",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Dance of the Fireflies",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Echoes of the Past",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Enhanted Dreams",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Sunlight Serenity",
+        src: "/assets/freedom.mp3",
+      },
+      {
+        title: "Misty Moonlight Melody",
+        src: "/assets/freedom.mp3",
+      },
+    ],
+  },
+];
+
+export const posts = [
+  {
+    id: "1",
+    date: "2025-05-10",
+    title: "Rock Concert in Central Park",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+  {
+    id: "2",
+    date: "2025-05-15",
+    title: "Jazz Jam Session",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+  {
+    id: "3",
+    date: "2025-05-20",
+    title: "EDM Extravaganza",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+  {
+    id: "4",
+    date: "2025-05-22",
+    title: "Accoustic Coffeehouse",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+  {
+    id: "5",
+    date: "2025-05-25",
+    title: "Classical Symphony Showcase",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+  {
+    id: "6",
+    date: "2025-05-27",
+    title: "Hip-Hop Danse Battle",
+    description:
+      "Join us for an electrifying night of classic rock hits at Central Park! The lineup includes tribute bands for legendary rock icons.",
+  },
+];
